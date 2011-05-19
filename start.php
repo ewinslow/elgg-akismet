4aa81da4a78d03e06b1affd05335b0f35eab2378
Evan Winslow <evan@elgg.org>
2011-05-19 01:18:14 -0700

    Don't want to complete the action + forward to a dead page, for example.

--- start.php
+++ start.php
@@ -31,6 +31,9 @@ function akismet_object_handler($event, $object_type, ElggObject $object) {
 		if (akismet_scan($comment, $author, $author_email, $author_url)) {
 			register_error(elgg_echo('akismet:spam'));
 			$object->disable('spam');
+			
+			//bail on the current action + return to previous page seems to make the most sense here...
+			forward(REFERER);
 		}
 	}
 }
