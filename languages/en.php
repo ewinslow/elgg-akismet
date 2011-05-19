887ab998bffe42d3431ec5afc9922595f2702265
Evan Winslow <evan@elgg.org>
2011-05-19 01:06:10 -0700

    Fixes #1: Getting akismet ready for a beta!

--- languages/en.php
+++ languages/en.php
@@ -1,8 +1,8 @@
 <?php
 
 add_translation("en", array(
-	'akismet:nowordpresskey' => 'Wordpress key not defined, please configure Akismet in your admin tools',
-	'akismet:wordpress_key' => 'Wordpress key',
+	'akismet:noapikey' => 'API key not defined, please configure Akismet in your admin tools',
+	'akismet:api_key' => 'API key',
 	'akismet:spam' => 'Sorry, you message could not be posted as Akismet thinks that it is spam!',
 	'akismet:ham' => 'Message passed Akismet spam check',
 ));
--- start.php
+++ start.php
@@ -7,9 +7,12 @@ function akismet_init() {
 	// Register for autoloading
 	elgg_register_class('Akismet', dirname(__FILE__) . "/vendors/akismet/PHP5Akismet.0.4/Akismet.class.php");
 
-	// Listen to events
-	elgg_register_event_handler('create', 'annotation', 'akismet_annotation_handler');
-	elgg_register_event_handler('create', 'object', 'akismet_object_handler');
+	// Only filter if the plugin has been set up
+	if (elgg_get_plugin_setting('api_key', 'akismet')) {
+		elgg_delete_admin_notice('akismet_key');
+		elgg_register_event_handler('create', 'annotation', 'akismet_annotation_handler');
+		elgg_register_event_handler('create', 'object', 'akismet_object_handler');
+	}
 }
 
 function akismet_annotation_handler($event, $object_type, ElggAnnotation $object) {
@@ -29,6 +32,8 @@ function akismet_annotation_handler($event, $object_type, ElggAnnotation $object
 			register_error(elgg_echo('akismet:spam'));
 
 			return false;
+		} else {
+			system_message(elgg_echo('akismet:ham'));
 		}
 	}
 }
@@ -50,6 +55,8 @@ function akismet_object_handler($event, $object_type, ElggObject $object) {
 		if (akismet_scan($comment, $author, $author_email, $author_url)) {
 			register_error(elgg_echo('akismet:spam'));
 			$object->disable('spam');
+		} else {
+			system_message(elgg_echo('akismet:ham'));
 		}
 	}
 }
@@ -66,15 +73,13 @@ function akismet_object_handler($event, $object_type, ElggObject $object) {
  * @return bool true if spam
  */
 function akismet_scan($comment, $author = "", $author_email = "", $author_url = "", $permlink = "") {
-	global $CONFIG;
+	$key = elgg_get_plugin_setting('api_key', 'akismet');
 
-	$key = $CONFIG->wordpress_key;
-	$url = $CONFIG->wwwroot;
+	if (!$key) {
+		throw new ConfigurationException(elgg_echo('akismet:noapikey'));
+	}
 
-	if (!$key)
-		throw new ConfigurationException(elgg_echo('akismet:nowordpresskey'));
-
-	$akismet = new Akismet($url, $key);
+	$akismet = new Akismet(elgg_get_site_url(), $key);
 	$akismet->setCommentAuthor($author);
 	$akismet->setCommentAuthorEmail($author_email);
 	$akismet->setCommentAuthorURL($author_url);
