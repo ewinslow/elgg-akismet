90790a295ee33714ce4679e04214760e24193d9d
Brett Profitt <brett.profitt@gmail.com>
2011-06-18 18:40:10 -0400

    Fixed incorrect language for ban_max_days.

--- languages/en.php
+++ languages/en.php
@@ -3,7 +3,7 @@
 add_translation("en", array(
 	'akismet:noapikey' => 'API key not defined, please configure Akismet in your admin tools',
 	'akismet:api_key' => 'API key',
-	'akismet:ban_max_days' => 'Only auto-ban users if their account is older than this many days',
+	'akismet:ban_max_days' => 'Only auto-ban users if their account is younger than this many days',
 	'akismet:spam' => 'You have been banned for spam! Please get in contact with an administrator if you feel this is in error.',
 	'akismet:ham' => 'Message passed Akismet spam check',
 ));
